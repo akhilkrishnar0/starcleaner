328ff0cfa98ce2d685de30ab4b8b36324c71991d
akhilkrishnar0 <akhil.r@res.christuniversity.in>
2025-11-18 20:57:32 +0530

    2 commit

--- starcleaner/__init__.py
+++ starcleaner/__init__.py
@@ -1,14 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="galnamefix",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        "pandas",
-        "astropy",
-        "astroquery"
-    ],
-    python_requires='>=3.11',
-)
-
+__version__ = "0.1"
+x
